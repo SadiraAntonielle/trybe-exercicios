32649e6a4538818c5b33a563110537b8198e56de
SadiraAntonielle <sadiraoliveira@gmail.com>
2022-09-22 09:20:37 -0300

    resolvendo o 3 exercicio

---
+++ fundamentos/secao-04/secao-4-introducao-a-javascript/dia-1-js-primeiros-passos/exercicio3.js
@@ -0,0 +1,12 @@
+const a = 9;
+const b = 15;
+const c = 10;
+
+if(a > b) {
+    console.log(" 'a' é maior que o 'b'");
+}
+else if(b > c) {
+    console.log(" 'b' é maior que o 'c'");
+} else {
+    console.log("O maior número é o 'c'");
+}
