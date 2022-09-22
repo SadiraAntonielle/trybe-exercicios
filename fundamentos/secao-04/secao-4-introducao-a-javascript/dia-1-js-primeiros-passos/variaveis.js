7c3b8aa55d0774c445c6fa54c0aed0abf6b51753
SadiraAntonielle <sadiraoliveira@gmail.com>
2022-09-22 08:57:30 -0300

    resolvendo 1 exercicio

---
+++ fundamentos/secao-04/secao-4-introducao-a-javascript/dia-1-js-primeiros-passos/variaveis.js
@@ -0,0 +1,11 @@
+const myName = "Sadira";
+const birthCity = "Silvianopolis";
+birthCity = "Pouso Alegre";
+let birthYear = 1994;
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+console.log(birthYear);
+console.log(birthCity);
