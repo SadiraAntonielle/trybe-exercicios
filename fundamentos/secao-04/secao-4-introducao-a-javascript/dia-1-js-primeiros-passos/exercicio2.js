c0e241b3a74ca301dcb5ba0b19070c8e663e8c36
SadiraAntonielle <sadiraoliveira@gmail.com>
2022-09-22 09:14:50 -0300

    resolvendo o 2 exercicio

---
+++ fundamentos/secao-04/secao-4-introducao-a-javascript/dia-1-js-primeiros-passos/exercicio2.js
@@ -0,0 +1,8 @@
+const primeiroValor = 6;
+const segundoValor = 12;
+
+if(primeiroValor > segundoValor) {
+    console.log("O primeiroValor é maior que o segundoValor");
+} else {
+    console.log("O segundoValor é maior que o primeiroValor");
+}
