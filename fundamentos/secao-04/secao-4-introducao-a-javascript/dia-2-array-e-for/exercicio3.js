c8091d0fba223786638ddad6e17f0914ad77651e
SadiraAntonielle <sadiraoliveira@gmail.com>
2022-09-26 09:48:15 -0300

    resovlendo 3 exericio dia 2

--- fundamentos/secao-04/secao-4-introducao-a-javascript/dia-2-array-e-for/exercicio3.js
+++ fundamentos/secao-04/secao-4-introducao-a-javascript/dia-2-array-e-for/exercicio3.js
@@ -3,10 +3,19 @@
 A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos. */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
 
 for (let index = 0; index < numbers.length; index += 1){
-    soma = soma = numbers[index];
+    soma = soma + numbers[index];
 }
 let media = soma / numbers.length;
 
 console.log(media);
+
+/* Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”; */
+
+if (media > 20) {
+    console.log("Valor maior que 20");
+} else {
+    console.log("Valor menor ou igual a 20");
+}
