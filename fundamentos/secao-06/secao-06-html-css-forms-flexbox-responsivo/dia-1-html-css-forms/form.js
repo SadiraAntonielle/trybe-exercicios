38c31a5076a06b09aaa4a6136e4481558bc9b8ba
SadiraAntonielle <sadiraoliveira@gmail.com>
2022-10-10 18:02:45 -0300

    dia 2 exercicio fixação

---
+++ fundamentos/secao-06/secao-06-html-css-forms-flexbox-responsivo/dia-1-html-css-forms/form.js
@@ -0,0 +1,13 @@
+const enviar = document.getElementById('enviar');
+const autorizarUsoImagem = document.getElementById('autorizacao');
+
+enviar.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+autorizarUsoImagem.addEventListener('click', (event) => {
+    if (autorizarUsoImagem )
+})
+
+
+
